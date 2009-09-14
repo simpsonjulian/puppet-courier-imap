43b96dd1776badef131f93703340c57c7f74fd1a
simpsonjulian <simpsonjulian@gmail.com>
2009-09-14 22:36:20 +0100

    extracted the imap server

---
+++ manifests/courier.pp
@@ -0,0 +1,11 @@
+class mailserver::imapd {
+  package { 
+      "courier-imap": ensure => installed;
+    }
+    service {
+      "courier-imap":
+        enable => true, 
+        ensure => running,
+        require => Package["courier-imap"];
+    }
+}
